cff2eb492547742b12304c5c6d8fc05211dd46b9
Hanna <anan_h200@hotmail.com>
2020-04-14 09:47:36 +0200

    Models actualizado

--- EMOCIOCLOWN/APPWEB/models.py
+++ EMOCIOCLOWN/APPWEB/models.py
@@ -4,59 +4,62 @@ from django.db import models
 
 class Taller(models.Model):
     titulo = models.CharField(max_length = 20, null = False)
-    descripcion = models.TextField(blank = False,null = False)
-    precio = models.FloatField(default = 10, null= True)
-    aforo = models.IntegerField(default = 10, null= True)
-    fecha = models.DateField(default = 10, null = True)
-    edad = models.IntegerField(default=10, null= True)  
+    descripcion = models.TextField(max_length = 300, null = False)
     imagen = models.ImageField(upload_to='static/img')
-    #agregar clave foranea de lugar 
-    #agregar clave foranea de calendario
-
-
+    
     def __str__(self):
         return self.titulo
 
-class Contacto(models.Model):
-    nombre1 = models.CharField(max_length = 20, null = False)
-    telefono1 = models.IntegerField(default= 10, null= False)
+class Calendario(models.Model):
+    titulo = models.CharField(max_length = 20, null = False)
+    descripcion = models.TextField(max_length = 300, null = False)
+    imagen = models.ImageField(upload_to='static/img')
+    aforo = models.IntegerField(default = 3, null= True)
+    fecha = models.DateField(null=True, blank=True, auto_now_add=True)
+    edad = models.IntegerField(default=10, null= True)
+    precio = models.FloatField(default = 10, null= True) 
 
-    nombre_Happy = models.CharField(max_length = 20, null = False)
-    telefono_Happy = models.IntegerField(default= 10, null= False)
-
-    def __str__(self):
-        return self.nombre1
-
+  
 class Campamento(models.Model):
     nombre = models.CharField(max_length = 50, null = False)
-    descripcion = models.TextField(blank = False,null = False)
-    #agregar clave foranea de calendario
-    #agregar clave foranea de contacto 
-    #agregar clave foranea de lugar  
-     
-     
-
-class Evento_actividad(models.Model):
+    descripcion = models.TextField(max_length = 300,null = False)
+    fecha_inicio = models.DateField(null=True, blank=True, auto_now_add=True)
+    fecha_fin = models.DateField(null=True, blank=True, auto_now_add=True) #preguntar a Isa
+    imagen = models.ImageField(upload_to='static/img')
+  
+ 
+class Opiniones(models.Model):
     nombre = models.CharField(max_length = 50, null = False)
     descripcion = models.TextField(blank = False,null = False)
-    #agregar clave foranea de calendario
-    #agregar clave foranea de contacto 
-    #agregar clave foranea de lugar 
+
+class Galeria(models.Model):
+    titulo = models.CharField(max_length = 50, null = False)
+    descripcion = models.TextField(blank = False,null = False)
+    imagen = models.ImageField(upload_to='static/img')
 
 
+class Blog(models.Model):
+    titular = models.CharField(max_length = 50, null = False)
+    resumen = models.TextField(blank = False,null = False)
+    descripcion = models.TextField(blank = False,null = False)
+    fecha = models.DateField(null=True, blank=True, auto_now_add=True)
+    tag = models.CharField(max_length = 50, null = False)
+    imagen = models.ImageField(upload_to='static/img')       
+        
 
-class Calendario(models.Model):
-    #agregar claves foraneas de: talleres, eventos, actividades, cammpamentos y contacto
-    mes = models.DateField(default = 10, null = True)
-    dia = models.DateField(default = 10, null = True)
-    hora = models.DateField(default = 10, null = True)
-    anio = models.DateField(default = 10, null = True) 
+class Datos_contacto(models.Model):
+    nombre = models.CharField(max_length = 20, null = False)
+    telefono = models.IntegerField(default= 9, null= False)
+    email = models.EmailField(max_length = 50, null = False)
+ 
 
+class Contacto(models.Model):
+    nombre = models.CharField(max_length = 20, null = False)
+    telefono = models.IntegerField(default= 9, null= False)
+    email = models.EmailField(max_length = 50, null = False)
+    asunto = models.CharField(max_length = 20, null = False)
+    mensaje = models.TextField(blank = False, null = False)
+     
+     
 
-class Lugar(models.Model):
-    ubicacion = models.CharField(max_length = 20, null = False)
-
-
-    #HAY QUE AGREGAR UNA CLASE "BLOG"????
-
-    #de "OPINIONES y GALERIA" hay que crear registros???
+  
