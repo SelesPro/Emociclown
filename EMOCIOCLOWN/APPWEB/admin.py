a1a9250b2f7cfea8f30e67f0077d261cbc547040
Hanna <anan_h200@hotmail.com>
2020-04-15 09:07:16 +0200

    admin creado

--- EMOCIOCLOWN/APPWEB/admin.py
+++ EMOCIOCLOWN/APPWEB/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Taller)
+admin.site.register(Evento)
+admin.site.register(Campamento)
+admin.site.register(Opiniones)
+admin.site.register(Galeria)
+admin.site.register(Blog)
+admin.site.register(Datos_contacto)
+admin.site.register(Contactar)
