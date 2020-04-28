7b328ea54e0b70783b1fdcbda3fb81161dc59fcb
daniel <dannyell22@yahoo.es>
2020-04-28 10:29:14 +0200

    Archivo models, class info, link
    Funcionalidad botón más información de inicio

--- EMOCIOCLOWN/APPWEB/views.py
+++ EMOCIOCLOWN/APPWEB/views.py
@@ -9,8 +9,8 @@ class Index(TemplateView):
 
     def get_context_data(self,**kwargs):
         context=super(Index, self).get_context_data(**kwargs)
-        #context['monica'] = Info.objects.get(pk=1)
-        #context['emocioclown'] = Info.objects.get(pk=2)
+        context['monica'] = Info.objects.get(pk=1)
+        context['emocioclown'] = Info.objects.get(pk=2)
         context['talleres'] = Taller.objects.all()
         context['eventos'] = Evento.objects.all()
         context['campamentos'] = Campamento.objects.all()
