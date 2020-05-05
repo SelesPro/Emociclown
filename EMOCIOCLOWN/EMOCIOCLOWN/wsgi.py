b1dbf728638a5393731b134ca37018d088051aa2
Elena Romero <registrospam@hotmail.com>
2020-05-05 18:43:18 +0200

    .

--- EMOCIOCLOWN/EMOCIOCLOWN/wsgi.py
+++ EMOCIOCLOWN/EMOCIOCLOWN/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('path/to/EMOCIOCLOWN/')
+sys.path.append('path/to/emocioclown/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EMOCIOCLOWN.settings')
 
 application = get_wsgi_application()
