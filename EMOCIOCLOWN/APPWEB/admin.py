0adef159434bee6375d551a1b233e5f741976361
Elena Romero <registrospam@hotmail.com>
2020-05-07 10:03:19 +0200

    Iconos Admin 1.0

--- EMOCIOCLOWN/APPWEB/admin.py
+++ EMOCIOCLOWN/APPWEB/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 from .models import *
 
 # Register your models here.
+"""
 admin.site.register(Info)
 admin.site.register(Taller)
 admin.site.register(Evento)
@@ -10,3 +12,36 @@ admin.site.register(Opiniones)
 admin.site.register(Galeria)
 admin.site.register(Blog)
 admin.site.register(Datos_contacto)
+"""
+
+@register(Info)
+class MaterialInfoAdmin(admin.ModelAdmin):
+    icon_name = 'person'
+
+@register(Taller)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'title'
+
+@register(Evento)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'date_range'
+
+@register(Campamento)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'change_history'
+
+@register(Opiniones)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'comment'
+
+@register(Galeria)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'local_see'
+
+@register(Blog)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'edit'
+
+@register(Datos_contacto)
+class MaterialContactAdmin(admin.ModelAdmin):
+    icon_name = 'local_post_office'
--- EMOCIOCLOWN/APPWEB/apps.py
+++ EMOCIOCLOWN/APPWEB/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class AppwebConfig(AppConfig):
     name = 'APPWEB'
+
+
